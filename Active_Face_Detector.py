08d1df611adb4809bc5189aadb2ffcb4e8b08015
jisoopark <jisoopark3@gmail.com>
2022-10-31 15:55:28 -0400

    Added face detection code

---
+++ Active_Face_Detector.py
@@ -0,0 +1,27 @@
+import cv2
+from random import randrange
+
+# This loads some pre-trained data on face frontal from opencv
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_defalut.xml')
+
+# To capture video from webcam.
+webcam = cv2.VideoCapture(0)
+
+# Iterate over frames
+while True:
+    # read the current frame
+    successful_frame_read, frame = webcam.read()
+    # Convert image to grayscale
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect Face => [[382 88 172 172]] = [[x, y, w, h]] -> Can detect multiple
+    face_location = trained_face_data.detectMultiScale(gray_img)
+
+    # Draw rectangle around face
+    for (x, y, w, h) in face_location:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
+
+    # Show image
+    cv2.imshow('Clever Face Detector', gray_img)
+    # Wait until image window closed
+    cv2.waitKey(1)
--- Face_Detector.py
+++ Face_Detector.py
@@ -1,19 +1,22 @@
 import cv2
+from random import randrange
 
 # This loads some pre-trained data on face frontal from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_defalut.xml')
 
 # We will choose an image to detect faces in
-img = cv2.imread('Queen.png')
+# img = cv2.imread('Queen.png')
+img = cv2.imread('Multi.png')
 # Convert image to grayscale
 gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# webcam = cv2.VideoCapture(0)
 
-# Detect Face => [[382 88 172 172]] = [[x, y, w, h]]
+# Detect Face => [[382 88 172 172]] = [[x, y, w, h]] -> Can detect multiple
 face_location = trained_face_data.detectMultiScale(gry_img)
 
 # Draw rectangle around face
 for (x, y, w, h) in face_location:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
 
 # Show image
 cv2.imshow('Clever Face Detector', img)
