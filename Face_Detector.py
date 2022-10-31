7ecfc1ad8870e0af60743021293efa0e043f2ffb
jisoopark <jisoopark3@gmail.com>
2022-10-31 15:32:18 -0400

    Added face detection code

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,11 +1,23 @@
 import cv2
 
-#This loads some pre-trained data on face frontals from opencv
+# This loads some pre-trained data on face frontal from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_defalut.xml')
 
 # We will choose an image to detect faces in
-img = cv2.imread('Face.JPG')
+img = cv2.imread('Queen.png')
+# Convert image to grayscale
+gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Detect Face => [[382 88 172 172]] = [[x, y, w, h]]
+face_location = trained_face_data.detectMultiScale(gry_img)
 
+# Draw rectangle around face
+for (x, y, w, h) in face_location:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# Show image
+cv2.imshow('Clever Face Detector', img)
+# Wait until image window closed
+cv2.waitKey()
 
 print("Code Complete")
