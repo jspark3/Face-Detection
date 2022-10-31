91c11765a846bfa921c34ea10a5db729d87533da
jisoopark <jisoopark3@gmail.com>
2022-10-31 15:09:25 -0400

    Added new image and pre trained data from open cv

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,5 +1,11 @@
 import cv2
 
+#This loads some pre-trained data on face frontals from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_defalut.xml')
+
+# We will choose an image to detect faces in
+img = cv2.imread('Face.JPG')
+
+
 
 print("Code Complete")
