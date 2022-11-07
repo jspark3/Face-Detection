d407e209cc581ed0d1a456f9e7316e159f26067a
jisoopark <jisoopark3@gmail.com>
2022-11-07 16:10:10 -0500

    SVD updated

--- Face_Detector.py
+++ Face_Detector.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 from random import randrange
 
+#################################
+####   IMG PROCESSING CODE   ####
+#################################
+
 # This loads some pre-trained data on face frontal from opencv
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_defalut.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 # We will choose an image to detect faces in
-img = cv2.imread('Queen.png')
+# img = cv2.imread('Queen.png')
 # Case 2 with multiple faces
-# img = cv2.imread('Multi.png')
+img = cv2.imread('Multi.png')
 
 # Convert image to grayscale
 gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,31 +20,43 @@ gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #################################
 #### FLATTENING METHOD 1 SVD ####
 #################################
+
 # calculate svd
 U, S, VT = np.linalg.svd(gry_img, full_matrices=False)
 # extracting diagonal Singular Values from the image
 S = np.diag(S)
 
 # Set r value
-r = 20
+# From r = 22, the accuracy goes down.
+r = 23
 # approximation
 reduced_img = U[:, :r]@S[0:r, :r]@VT[:r, :]
+########## Figure out how long this takes ##########
 
+#################################
+#### FLATTENING METHOD 1 SVD ####
+#################################
+
+
+#################################
+####   FACE DETECTION CODE   ####
+#################################
 
 # Convert type of image to uint8
 reduced_img = np.array(reduced_img, dtype='uint8')
+# reduced_img = np.array(gry_img, dtype='uint8')
 
 # Detect Face => [[382 88 172 172]] = [[x, y, w, h]] -> Can detect multiple
 face_location = trained_face_data.detectMultiScale(reduced_img)
 
 print(face_location)
 
-# Draw rectangle around face
+# Using coordinates above draw a box around the face on the colored image
 for (x, y, w, h) in face_location:
-    cv2.rectangle(reduced_img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
 
 # Show image
-cv2.imshow('Clever Face Detector', reduced_img)
+cv2.imshow('Clever Face Detector', img)
 # Wait until image window closed
 cv2.waitKey()
 
