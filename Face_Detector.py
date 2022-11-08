68b566e20b33b1493a9817f305dae7c353a9d651
jisoopark <jisoopark3@gmail.com>
2022-11-08 11:46:12 -0500

    Decomposition modified

--- Face_Detector.py
+++ Face_Detector.py
@@ -7,12 +7,13 @@ from random import randrange
 #################################
 
 # This loads some pre-trained data on face frontal from opencv
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
 # We will choose an image to detect faces in
 # img = cv2.imread('Queen.png')
 # Case 2 with multiple faces
 img = cv2.imread('Multi.png')
+# img = cv2.imread('Multi_2.png')
 
 # Convert image to grayscale
 gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -28,7 +29,7 @@ S = np.diag(S)
 
 # Set r value
 # From r = 22, the accuracy goes down.
-r = 23
+r = 25
 # approximation
 reduced_img = U[:, :r]@S[0:r, :r]@VT[:r, :]
 ########## Figure out how long this takes ##########
@@ -48,8 +49,6 @@ reduced_img = np.array(reduced_img, dtype='uint8')
 
 # Detect Face => [[382 88 172 172]] = [[x, y, w, h]] -> Can detect multiple
 face_location = trained_face_data.detectMultiScale(reduced_img)
-
-print(face_location)
 
 # Using coordinates above draw a box around the face on the colored image
 for (x, y, w, h) in face_location:
